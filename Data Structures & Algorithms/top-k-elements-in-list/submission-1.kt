ed7593019c7262b52cb048d79b3c6fef4e54d6dd
Максим <114164405+sharingan2077@users.noreply.github.com>
2026-07-23 11:24:09 +0300

    Add: top-k-elements-in-list - submission-1

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-1.kt
@@ -0,0 +1,24 @@
+class Solution {
+    fun topKFrequent(nums: IntArray, k: Int): IntArray {
+        val arr = IntArray(k)
+        val buckets = MutableList<MutableList<Int>>(nums.size + 1) { mutableListOf() }
+        val map = nums.groupBy { it }.mapValues { it.value.size }
+        for ((k, v) in map) {
+            buckets[v].add(k)
+        }
+        var count = 0
+        for (i in buckets.size - 1 downTo 0) {
+
+            for (j in buckets[i].indices) {
+                arr[count] = buckets[i][j]
+                count++
+                if (count == k) {
+                    return arr
+                }
+            }
+
+        }
+        return arr
+
+    }
+}
