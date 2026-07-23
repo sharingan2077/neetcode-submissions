a3f0fe7c14361de2c77b81eb395fc44d9a740a4a
Максим <114164405+sharingan2077@users.noreply.github.com>
2026-07-23 14:56:18 +0300

    Add: valid-sudoku - submission-1

---
+++ Data Structures & Algorithms/valid-sudoku/submission-1.kt
@@ -0,0 +1,54 @@
+class Solution {
+    fun isValidSudoku(board: Array<CharArray>): Boolean {
+        val rowArr = IntArray(10)
+        val colArr = IntArray(10)
+        for (i in board.indices) {
+            for (j in board.indices) {
+                if (board[i][j] != '.') {
+                    if (rowArr[board[i][j] - '0'] != 0) {
+                        return false
+                    }
+                    rowArr[board[i][j] - '0'] = 1
+                }
+                if (board[j][i] != '.') {
+                    if (colArr[board[j][i] - '0'] != 0) {
+                        return false
+                    }
+                    colArr[board[j][i] - '0'] = 1
+                }
+            }
+            rowArr.fill(0)
+            colArr.fill(0)
+        }
+        for (i in 0..6 step 3) {
+            for (j in 0..6 step 3) {
+                val res = helper(board, i, j)
+                if (!res) return false
+            }
+        }
+        return true
+    }
+
+    private fun helper(board: Array<CharArray>, i: Int, j: Int): Boolean {
+        val arr = IntArray(10)
+        return secondHelper(board, arr, i, j) &&
+                secondHelper(board, arr, i, j + 1) &&
+                secondHelper(board, arr, i, j + 2) &&
+                secondHelper(board, arr, i + 1, j) &&
+                secondHelper(board, arr, i + 1, j + 1) &&
+                secondHelper(board, arr, i + 1, j + 2) &&
+                secondHelper(board, arr, i + 2, j) &&
+                secondHelper(board, arr, i + 2, j + 1) &&
+                secondHelper(board, arr, i + 2, j + 2)
+    }
+
+    private fun secondHelper(board: Array<CharArray>, arr: IntArray, i: Int, j: Int): Boolean {
+        if (board[i][j] != '.') {
+            if (arr[board[i][j] - '0'] != 0) {
+                return false
+            }
+            arr[board[i][j] - '0'] = 1
+        }
+        return true
+    }
+}
