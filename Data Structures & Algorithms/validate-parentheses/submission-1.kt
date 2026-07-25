0d079902318d6989deac12031d928f0b241a1730
Максим <114164405+sharingan2077@users.noreply.github.com>
2026-07-25 23:10:12 +0300

    Add: validate-parentheses - submission-1

---
+++ Data Structures & Algorithms/validate-parentheses/submission-1.kt
@@ -0,0 +1,17 @@
+class Solution {
+    fun isValid(s: String): Boolean {
+        val map = mapOf(')' to '(', '}' to '{', ']' to '[')
+        val stack = ArrayDeque<Char>()
+
+        for (ch in s) {
+            if (ch in map) {
+                if (stack.isEmpty() || stack.removeLast() != map[ch]) {
+                    return false
+                }
+            }
+            else stack.add(ch)
+        }
+        return stack.isEmpty()
+
+    }
+}
