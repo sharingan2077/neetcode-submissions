f12c433c4c8b4f9f00cf3981eeb1481d230658a1
Максим <114164405+sharingan2077@users.noreply.github.com>
2026-07-26 11:29:12 +0300

    Add: binary-search - submission-0

---
+++ Data Structures & Algorithms/binary-search/submission-0.kt
@@ -0,0 +1,22 @@
+class Solution {
+    fun search(nums: IntArray, target: Int): Int {
+        var start = 0
+        var end = nums.size - 1
+
+
+        while (start <= end) {
+            val middle = start + (end - start) / 2
+            if (nums[middle] == target) {
+                return middle
+            }
+            else if (nums[middle] < target) {
+                start = middle + 1
+            }
+            else {
+                end = middle - 1
+            }
+        }
+        return -1
+
+    }
+}
