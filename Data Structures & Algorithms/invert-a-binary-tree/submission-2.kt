5537e835e8d8e84c1f422b21e23ebcf8b13c0bcc
Максим <114164405+sharingan2077@users.noreply.github.com>
2026-08-03 23:34:49 +0300

    Add: invert-a-binary-tree - submission-2

---
+++ Data Structures & Algorithms/invert-a-binary-tree/submission-2.kt
@@ -0,0 +1,32 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode(var `val`: Int) {
+ *     var left: TreeNode? = null
+ *     var right: TreeNode? = null
+ * }
+ */
+
+class Solution {
+    fun invertTree(root: TreeNode?): TreeNode? {
+        if (root == null) {
+            return null
+        }
+        val queue: ArrayDeque<TreeNode> = ArrayDeque()
+        queue.add(root)
+
+        while (queue.isNotEmpty()) {
+            val node = queue.removeLast()
+
+                val temp = node.left
+                node.left = node.right
+                node.right = temp
+                if (node.left != null) {
+                    queue.add(node.left)
+                }
+                if (node.right != null) {
+                    queue.add(node.right)
+                }
+        }
+        return root
+    }
+}
